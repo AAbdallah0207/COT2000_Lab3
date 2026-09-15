c840489bbd7dfd99bf5c8adea7b84e231a93077c
Ali Abdallah <aabdallah2024@fau.edu>
2026-09-15 14:25:23 -0400

    Add additional variables and print statements
    Added team, coin_flip, and car variables with print statements.

--- Lab3.py
+++ Lab3.py
@@ -1,4 +1,10 @@
 name = "Ali Abdallah"
 numbers = {1, 2, 3, 4, 5}
+team = "Real Madrid"
+coin_flip = {"Head", "Tail"}
+car = "2021 Volkswagon Jetta"
 print("Student:", name)
 print("Numbers:", numbers)
+print("Team Name:", team)
+print("Coin Flip:", coin_flip)
+print("Car:", car)
